cbff8459a0ce20ae696099e5591279e2214004d8
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2022-06-04 22:22:02 +0200

    chore(app): added connect to wallet

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { WalletService } from './services/wallet.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng-connect-ethereum-wallet';
+
+  public walletConnected: boolean = false;
+  public walletId: string = '';
+
+  constructor(private walletService: WalletService){
+
+  }
+
+  connectToWallet  = () => {
+    this.walletService.connectWallet();
+  }
+
+  checkWalletConnected = async () => {
+    const accounts = await this.walletService.checkWalletConnected();
+    if(accounts.length > 0){
+      this.walletConnected = true;
+      this.walletId = accounts[0];
+    }
+  }
+
+  ngOnInit(): void {
+    this.connectToWallet();
+    this.checkWalletConnected();
+  }
+}
